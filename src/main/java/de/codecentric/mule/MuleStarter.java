d0c7be77e43968759300bfa1ef40e53973a9653c
roger.butenuth <roger.butenuth@codecentric.de>
2020-10-15 19:49:57 +0200

    Set MULE_HOME if not set, wait for termination when stopping.
    (sometimes the debug port is still blocked when immediately restarting)

--- src/main/java/de/codecentric/mule/MuleStarter.java
+++ src/main/java/de/codecentric/mule/MuleStarter.java
@@ -2,8 +2,15 @@ package de.codecentric.mule;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class MuleStarter {
 
@@ -13,7 +20,11 @@ public class MuleStarter {
 		if (config.isStop()) {
 			try {
 				Socket s = new Socket("localhost", config.getPort());
+				InputStream is = s.getInputStream();
+				int b = is.read();
+				is.close();
 				s.close();
+				System.out.println("stopped Mule, value was: " + b);
 			} catch (IOException e) {
 				System.out.println("Stopping via socket connect failed: " + e.getMessage());
 			}
@@ -64,9 +75,11 @@ public class MuleStarter {
 
 	private static void startMule(Configuration config) throws Exception {
 		String[] cmdarray = config.getMuleCommandWithArguments().toArray(new String[0]);
-		
-		Process p = Runtime.getRuntime().exec(cmdarray, null /* String[] env, null defaults to env of current VM */,
-				config.getMuleHome() /* working dir */);
+		Map<String, String> env = new HashMap<>(System.getenv());
+		if (!env.containsKey("MULE_HOME")) {
+			env.put("MULE_HOME", config.getMuleHomePath());
+		}
+		Process p = Runtime.getRuntime().exec(cmdarray, environment(env), config.getMuleHome() /* working dir */);
 		TextForwarder stderr = new TextForwarder(p.getErrorStream(), System.err, "stderr");
 		stderr.start();
 		TextForwarder stdout = new TextForwarder(p.getInputStream(), System.out, "stdout");
@@ -79,18 +92,30 @@ public class MuleStarter {
 			}
 
 		});
-		waitOnSocketForShutdown(config);
-		p.destroy();
-		p.waitFor();
+		waitOnSocketForShutdown(config, p);
 		stdout.join();
 		stderr.join();
-		System.out.println("Mule has stopped.");
 	}
 
-	private static void waitOnSocketForShutdown(Configuration config) throws IOException {
+	private static String[] environment(Map<String, String> env) {
+		List<String> envList = new ArrayList<>();
+		for (Entry<String, String> e : env.entrySet()) {
+			envList.add(e.getKey() + "=" + e.getValue());
+		}
+		return envList.toArray(new String[0]);
+	}
+
+	private static void waitOnSocketForShutdown(Configuration config, Process p) throws Exception {
 		System.out.println("Wait for socket connection on port " + config.getPort());
 		ServerSocket socket = new ServerSocket(config.getPort());
 		Socket s = socket.accept();
+		p.destroy();
+		p.waitFor();
+		System.out.println("Mule has stopped.");
+		OutputStream os = s.getOutputStream();
+		os.write(42);
+		os.flush();
+		os.close();
 		s.close();
 		socket.close();
 	}
